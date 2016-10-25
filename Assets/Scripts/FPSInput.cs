b7759d5308afb32af03cbd5990f47ce450005324
Ruslan Durov <s.t.o.k.a.t.p@gmail.com>
2016-10-25 17:37:38 +0300

    Добавьте файлы проекта.

---
+++ Assets/Scripts/FPSInput.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Control Scripts/FPS Input")]
+public class FPSInput : MonoBehaviour {
+
+    private CharacterController _characterController;
+
+    public float speed = 6.0f;
+    public float gravity = -9.8f;
+
+    // Use this for initialization
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+
+        movement = Vector3.ClampMagnitude(movement, speed); // Ограничим движение по диагонали той же скоростью, что и движени по осям
+        movement.y = gravity;
+
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement); // Преобразуем вектор к глобальным переменным
+
+        _characterController.Move(movement);
+
+        //transform.Translate(deltaX * Time.deltaTime, 0, deltaZ * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/RayShooter.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayShooter : MonoBehaviour {
+
+    private Camera _camera;
+
+	// Use this for initialization
+	void Start () {
+        _camera = GetComponent<Camera>();
+
+        Cursor.lockState = CursorLockMode.Locked; // Скрываем указатель мыши
+        Cursor.visible = false;
+	}
+
+    void OnGUI()
+    {
+        int size = 12;
+
+        float posX = _camera.pixelWidth / 2 - size / 4;
+        float posY = _camera.pixelHeight * 0.5f - size * 0.5f;
+
+        GUI.Label(new Rect(posX, posY, size, size), "*");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 point = new Vector3(_camera.pixelWidth * 0.5f, _camera.pixelHeight * 0.5f, 0);
+
+            Ray ray = _camera.ScreenPointToRay(point); ;
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                StartCoroutine(SphereIndicator(hit.point));
+            }
+        }
+	}
+
+    private IEnumerator SphereIndicator(Vector3 pos)
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+        sphere.transform.position = pos;
+
+        yield return new WaitForSeconds(1);
+
+        Destroy(sphere);
+    }
+}
